5e72bff80bf3f1201fdc8d22b30719ccfa905662
Fediory <f-he@163.com>
2024-03-28 05:53:18 +0800

    feat: testing cases

---
+++ user/loongarch64/run-all.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+tests="
+brk
+chdir
+clone
+close
+dup2
+dup
+execve
+exit
+fork
+getcwd
+getdents
+getpid
+getppid
+gettimeofday
+mkdir_
+mmap
+mount
+munmap
+openat
+open
+pipe
+read
+times
+umount
+uname
+unlink
+wait
+waitpid
+write
+yield
+"
+for i in $tests
+do
+	echo "Testing $i :"
+	./$i
+done
