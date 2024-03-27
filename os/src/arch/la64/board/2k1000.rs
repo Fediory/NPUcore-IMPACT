67fd15b8c18f5894815b4ab6eb7d14f5648bff10
Fediory <f-he@163.com>
2024-03-28 04:27:06 +0800

    del: NAND in 2k1000

--- os/src/arch/la64/board/2k1000.rs
+++ os/src/arch/la64/board/2k1000.rs
@@ -1,20 +1,12 @@
-use crate::config::HIGH_BASE_EIGHT;
-//use core::ptr::write_volatile;
+
 use crate::drivers::block::MemBlockWrapper;
-use super::ls_nand::{self, NandType};
 pub const MMIO: &[(usize, usize)] = &[];
 
 pub type BlockDeviceImpl = MemBlockWrapper;
 
 
 
-pub const NAND_TYPE: NandType = NandType::Sz2GbPg2K;
-pub const NAND_BASE: usize = 0x1fe2_6000 | HIGH_BASE_EIGHT;
 pub const DMA_ADDR: usize = 0x1fe0_0c00 | HIGH_BASE_EIGHT;
 pub const ROOT_BASE_ADDR: usize = 0x00e0_0000;
 pub const BLOCK_SZ: usize = 2048;
-pub const UART_MUX: usize = 2;
-pub const UART_BASE: usize = 0x0000_0000_1fe2_0000 + HIGH_BASE_EIGHT + {
-    assert!(UART_MUX < 10);
-    0x400 * UART_MUX
-};
+pub const UART_BASE: usize = 0x1fe2_0000;
